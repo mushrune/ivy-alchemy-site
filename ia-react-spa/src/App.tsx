605d27fe8632a2e6fa0707a8a9f71939bb6e36d0
mushrune <112779587+mushrune@users.noreply.github.com>
2023-11-09 09:56:23 -0800

    Added effect to ensure top of page is rendered between path changes, improved site metadata in public folder.

--- ia-react-spa/src/App.tsx
+++ ia-react-spa/src/App.tsx
@@ -1,11 +1,17 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import Navigator from "./Components/Navigator";
-import { Outlet } from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Footer from "./Components/Footer";
 
 // Renders the application and any elements that persist throughout.
 // The height of the Outlet element is managed such that it is consistent throughout the site.
 const App: React.FC = () => {
+    const location = useLocation();
+
+    useEffect( () => {
+        window.scrollTo(0, 0);
+    }, [location.pathname])
+
     return (
         <div className="App flex flex-col min-h-screen">
             <Navigator />
