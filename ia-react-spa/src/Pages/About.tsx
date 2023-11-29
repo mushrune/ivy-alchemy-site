c705b8abd7101fecf3eb09a20193751dde1ea0c3
mushrune <112779587+mushrune@users.noreply.github.com>
2023-11-28 16:39:53 -0800

    Updated about text

--- ia-react-spa/src/Pages/About.tsx
+++ ia-react-spa/src/Pages/About.tsx
@@ -3,24 +3,9 @@ import { TbInfoHexagon } from "react-icons/tb";
 import PageContainer from "../Components/PageContainer";
 import DocumentViewer from "../Components/DocumentViewer";
 
-const greetings: string[] = [
-    "Meow,",
-    "Hello,",
-    "Hi,",
-    "Well,",
-    "Sup,",
-    "Help,",
-    "Uhm,",
-    "Hey there,",
-    "Help!"
-];
-
 const About: React.FC = () => {
-
-    const greeting = greetings[ Math.floor( Math.random() * greetings.length ) ]
-
     return(
-        <PageContainer title={`${greeting} I'm Ivy.`} icon={<TbInfoHexagon size={40} className="ml-2" />}>
+        <PageContainer title="About" icon={<TbInfoHexagon size={40} className="ml-2" />}>
             <div className="mt-4 max-w-md mx-auto">
                 <img src="./ivy.jpeg" alt="ivy alchemist" className="w-full rounded-lg" />
             </div>
