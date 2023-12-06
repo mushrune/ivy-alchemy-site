fbaaf205fb6e6e18cd236eba405bf718a5136110
mushrune <112779587+mushrune@users.noreply.github.com>
2023-12-05 16:23:37 -0800

    Updates to ripley.png. Smaller, optimized for display on page load.

--- ia-react-spa/src/Components/Widgets/LeafIcon.tsx
+++ ia-react-spa/src/Components/Widgets/LeafIcon.tsx
@@ -6,34 +6,34 @@ type ItemWeight = {
     item: ReactElement
 }
 
-const items: ItemWeight[] = [
-    {
-        weight: 7777,
-        item: ( <TbLeaf size={25} /> )
-    },
-    {
-        weight: 7777,
-        item: ( <TbSeeding size={25} /> )
-    },
-    {
-        weight: 777,
-        item: ( <img src="./ripley.png" alt="ripley" style={{ width: 20, filter: "grayscale(100%) invert(90%) sepia(15%) saturate(1157%) hue-rotate(75deg) brightness(99%) contrast(89%)" }} /> )
-    },
-    {
-        weight: 1,
-        item: ( <TbBrandAmongUs size={25} /> )
-    },
-    {
-        weight: 7777,
-        item: ( <TbHeart size={25} /> )
-    }
-]
-
 interface props {
     size: number;
 }
 
-const LeafIcon: React.FC<props> = ({size}) => {
+const LeafIcon: React.FC<props> = ({ size }) => {
+    const items: ItemWeight[] = [
+        {
+            weight: 7777,
+            item: ( <TbLeaf size={size} /> )
+        },
+        {
+            weight: 7777,
+            item: ( <TbSeeding size={size} /> )
+        },
+        {
+            weight: 777,
+            item: ( <img src="/ripley.webp" alt="ripley" style={{ width: size - 5, filter: "grayscale(100%) invert(90%) sepia(15%) saturate(1157%) hue-rotate(75deg) brightness(99%) contrast(89%)" }} /> )
+        },
+        {
+            weight: 1,
+            item: ( <TbBrandAmongUs size={size} /> )
+        },
+        {
+            weight: 7777,
+            item: ( <TbHeart size={size} /> )
+        }
+    ]
+
     function randomItemSelect( items: ItemWeight[] ): number {
         // Calculate total weight for all items
         const totalWeight = items.reduce( ( sum, item ) => sum + item.weight, 0 )
--- ia-react-spa/src/Components/Widgets/LoadingWidget.tsx
+++ ia-react-spa/src/Components/Widgets/LoadingWidget.tsx
@@ -3,8 +3,8 @@ import {CustomLinearProgress} from "./CustomLinearProgress";
 import React from "react";
 
 const LoadingWidget: React.FC = () => { return(
-    <div className="m-auto mt-32 mb-40 w-fit flex flex-col items-center">
-        <img src="./ripley.png" alt="ripley" className="w-20 mt-4 animate-pulse"
+    <div className="m-auto mt-32 mb-40 w-fit flex flex-col items-center select-none">
+        <img src="./ripley.webp" alt="ripley" className="w-20 mt-4 animate-pulse"
              style={{filter: "grayscale(100%) invert(90%) sepia(15%) saturate(1157%) hue-rotate(75deg) brightness(99%) contrast(89%)"}}/>
         <Typography variant="h5" color="primary" className="m-2">loading...</Typography>
         <CustomLinearProgress className="w-full"/>
