c5ff434a236f24706f070b6e9e7bf33bb4e4e1bb
mushrune <112779587+mushrune@users.noreply.github.com>
2024-02-23 08:54:41 -0800

    Added 'new' corner decoration for new flash sheets.

--- ia-react-spa/src/Pages/Home/Components/FlashContainer.tsx
+++ ia-react-spa/src/Pages/Home/Components/FlashContainer.tsx
@@ -4,6 +4,9 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 import LeafSeparator from "../../../Components/Widgets/LeafSeparator";
 import { FlashSheet } from "../../../Types";
 import FlashController from "./FlashController";
+import {Typography} from "@mui/material";
+
+const msPerDay = 1000 * 60 * 60 * 24;
 
 interface props {
     flashSheet: FlashSheet;
@@ -24,8 +27,10 @@ function getRandomFramePath(): string {
 
 const FlashContainer: React.FC<props> = ({ flashSheet, showDivider, index, tone }) => {
 
+    // isNew is true if the flash sheet is less than 31 days old
+    const isNew: boolean = new Date(flashSheet.created_date).getTime() > ( new Date().getTime() - 31 * msPerDay );
     // bool that shows if the sheet's pieces have been filtered. The component behaves differently if this is true.
-    const isFiltered = flashSheet.piece_ids.length > flashSheet.flash_pieces.length
+    const isFiltered: boolean = flashSheet.piece_ids.length > flashSheet.flash_pieces.length
     const framePath: string = useMemo(() => getRandomFramePath(),[] );
 
     const [ flashSheetState, setFlashSheetState ] = useState<FlashSheet>( flashSheet );
@@ -36,10 +41,18 @@ const FlashContainer: React.FC<props> = ({ flashSheet, showDivider, index, tone 
 
     return(
         <div className={`
-                w-full h-fit overflow-hidden p-0 sm:px-4 
+                overall-flash-sheet-container
+                w-full h-fit overflow-hidden p-0 sm:px-4
+                relative
                 ${ !showDivider ? "pb-4" : "pb-2" }
             `}
-            key={index}>
+            key={index}
+        >
+            { isNew &&
+                <div className="absolute -right-4 bg-red-500 -top-4 w-[70px] h-[70px] z-[100] select-none rounded-full">
+                    <Typography sx={{ transform: 'rotate(45deg)'}} className="ml-[10px] w-fit h-fit my-[25px] text-[18px]" variant="subtitle2" >new!</Typography>
+                </div>
+            }
             <CarouselProvider
                 naturalSlideHeight={30}
                 naturalSlideWidth={30}
